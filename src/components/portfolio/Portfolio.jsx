e6fa480f653da3f1d64a216dfb5c1d93382ef2aa
Gagandeep Bhardwaj <gbhardwaj572@gmail.com>
2023-07-09 22:43:23 +0530

    experience page completed

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 import './portfolio.css'
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>My Portfolio</h2>
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="" className='btn'>GitHub Repo</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
